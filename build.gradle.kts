4f4402b0d5e8acf61d0f22ef14b5adf2e719950b
David Wursteisen <david.wursteisen+git@gmail.com>
2020-02-26 13:34:21 +0100

    Kotlin 1.3.61

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.3.60"
+    kotlin("multiplatform") version "1.3.61"
     id("maven-publish")
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
