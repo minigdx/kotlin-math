53805f77af945b33f318674b9bcd9d2c0fbe4c18
David Wursteisen <david.wursteisen+git@gmail.com>
2020-06-11 12:52:27 +0200

    [RELEASE] Prepare release with publishing on bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import java.util.Date
+import java.util.Properties
+
 plugins {
     kotlin("multiplatform") version "1.3.70"
     id("maven-publish")
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 group = "com.github.dwursteisen.kotlin-math"
@@ -17,16 +21,6 @@ repositories {
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/dwursteisen/kotlin-math")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
     publications {
         create<MavenPublication>("default") {
             from(components["kotlin"])
@@ -120,5 +114,51 @@ kotlin {
         val macosX64Main by getting {
             dependencies { }
         }
+    }
+}
+
+val properties = Properties()
+if (project.file("local.properties").exists()) {
+    properties.load(project.file("local.properties").inputStream())
+}
+
+configure<com.jfrog.bintray.gradle.BintrayExtension> {
+    user = properties.getProperty("bintray.user") ?: project.property("bintray_user") as? String ?: System.getProperty("BINTRAY_USER")
+    key = properties.getProperty("bintray.key") ?: project.property("bintray_user") as? String ?: System.getProperty("BINTRAY_KEY")
+    publish = true
+    setPublications("maven")
+    pkg(delegateClosureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+        repo = "minigdx"
+        name = project.name
+        githubRepo = "dwursteisen/kotlin-math.git"
+        vcsUrl = "https://github.com/dwursteisen/kotlin-math.git"
+        description = project.description
+        setLabels("java")
+        setLicenses("Apache 2.0")
+        desc = description
+        version(closureOf<com.jfrog.bintray.gradle.BintrayExtension.VersionConfig> {
+            this.name = project.version.toString()
+            released = Date().toString()
+        })
+    })
+}
+
+tasks.named("bintrayUpload") {
+    dependsOn(":publishToMavenLocal")
+}
+
+tasks.withType<com.jfrog.bintray.gradle.tasks.BintrayUploadTask> {
+    doFirst {
+        project.publishing.publications
+            .filterIsInstance<MavenPublication>()
+            .forEach { publication ->
+                val moduleFile = buildDir.resolve("publications/${publication.name}/module.json")
+                if (moduleFile.exists()) {
+                    publication.artifact(object :
+                        org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact(moduleFile) {
+                        override fun getDefaultExtension() = "module"
+                    })
+                }
+            }
     }
 }
