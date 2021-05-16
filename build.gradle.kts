8979456098dddd7f18e80ce553e421ed3b2bec4a
dwursteisen <david.wursteisen+git@gmail.com>
2021-05-16 19:57:39 +0200

    Bump to LATEST-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.minigdx.gradle.plugin.developer.mpp") version "1.0.0"
+    id("com.github.minigdx.gradle.plugin.developer.mpp") version "LATEST-SNAPSHOT"
 }
 
 repositories {
