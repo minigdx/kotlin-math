2528611e9441a30e7f7bb05393afc374cbbdcda7
dwursteisen <david.wursteisen+git@gmail.com>
2021-05-16 20:06:46 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.minigdx.gradle.plugin.developer.mpp") version "LATEST-SNAPSHOT"
+    id("com.github.minigdx.gradle.plugin.developer.mpp") version "1.1.0-alpha0"
 }
 
 repositories {
